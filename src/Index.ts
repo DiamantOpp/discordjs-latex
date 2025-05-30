c18525b949ac34f00540ed05a362044d194cae1d
みず <165909061+DiamantOpp@users.noreply.github.com>
2025-05-30 19:08:08 +0200

    Initial commit

---
+++ src/Index.ts
@@ -0,0 +1,82 @@
+import { Client, Events, GatewayIntentBits, Partials, REST, Routes, Interaction, SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+// Some helper methods for formatting -> better output/debugging readability
+const log = (...args: string[]) => console.log(`[LOG] ${args.join(' ')}`);
+const info = (...args: string[]) => console.info(`[INFO] ${args.join(' ')}`);
+const warn = (...args: string[]) => console.warn(`[WARN] ${args.join(' ')}`);
+const debug = (...args: string[]) => console.debug(`[DEBUG] ${args.join(' ')}`);
+const error = (...args: string[]) => {
+    console.error(`[ERROR] ${args}`);
+    return new Error(args.join(' '));
+}
+const assert = (condition: any, ...args: string[]) => {
+    if (!condition)
+        return console.error(`[ASSERT] ${args.join(' ')}`);
+}
+
+dotenv.config();
+
+assert(process.env.TOKEN,     'TOKEN is not defined'    );
+assert(process.env.CLIENT_ID, 'CLIENT_ID is not defined');
+
+type command = {
+    data: SlashCommandBuilder,
+    execute: (interaction: ChatInputCommandInteraction, properties: {[key: string]: any}) => void | any
+};
+
+const commands: command[] = [];
+const commandRef: {[key: string]: command} = {};
+info('Importing commands...');
+readdirSync(join(__dirname, 'commands')).forEach((module: string) => {
+    debug(`Importing command ${module}`);
+    const command = require(join(__dirname, 'commands', module));
+    if (!command.data)
+        return error(`Command ${module} does not have a data field, defaulting to empty.`);
+    if (!command.data.name) {
+        warn(`Command ${module} does not have a name, defaulting to filename.`);
+        command.data = command.data.setName(module.split('.')[0]);
+    }
+    commandRef[command.data.name] = command;
+    commands.push(command.data.toJSON());
+});
+
+const client = new Client({
+    intents: Object.values(GatewayIntentBits) as GatewayIntentBits[], // all intents  (me being lazy)
+    partials: Object.values(Partials) as Partials[],                  // all partials (me being lazy)
+});
+
+client.once(Events.ClientReady, () => {
+    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN?? '');
+    info('Refreshing application (/) commands.');
+    rest.put(Routes.applicationCommands(process.env.CLIENT_ID?? ''), { body: commands })
+        .then(() => info('Successfully refreshed application (/) commands.'))
+        .catch(err => error('Failed to refresh application (/) commands:', err));
+    info(`${client.user?.tag} is ready!`);
+});
+
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = commandRef[interaction.commandName];
+    if (command) {
+        log(`@${interaction.user.tag} ran /${interaction.commandName}`);
+        if (command.execute)
+            try {
+                await command.execute(
+                    interaction,
+                    interaction.commandName === 'ping'? {client: client}:
+                    {}
+                );
+            } catch (err) {
+                error(`Command ${interaction.commandName} failed:`, err);
+                await interaction.reply({content: `:x: Command failed: ${err}`});
+            }
+        else
+            await interaction.reply({content: `:x: ${interaction.commandName??''} is missing field \`execute\``});
+    } else await interaction.reply({content: `:x: ${interaction.commandName??''} is missing field \`this\``});
+});
+
+client.login(process.env.TOKEN);
---
+++ src/commands/latex.ts
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import sharp from 'sharp';
+import fs from 'fs';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+          .setName('latex')
+          .setDescription('Generate LaTeX markup.')
+          .setIntegrationTypes(0, 1)
+          .setContexts(0, 1, 2)
+          .addStringOption(option =>
+                option.setName('source')
+                      .setDescription('LaTeX source')
+                      .setRequired(true)
+          ),
+    execute: async (interaction: ChatInputCommandInteraction, _: {}) => {
+        const source = interaction.options.getString('source');
+        if (!source) return interaction.reply({content: ':x: No LaTeX provided.', ephemeral: true});
+        
+        const res = await fetch(`https://latex.codecogs.com/png.image?\\dpi{500} ${source}`);
+        if (!res.ok && res.status !== 400) return interaction.reply(`:x: Resource returned HTTP ${res.status} ${res.statusText}`);
+
+        const buffer = Buffer.from(await res.arrayBuffer());
+
+        fs.writeFileSync('latest.png', buffer);
+
+        const { data, info } = await sharp('latest.png').raw().toBuffer({ resolveWithObject: true });
+        for (let i = 0; i < data.length; i += info.channels) {
+            data[i  ] = 0xf2;
+            data[i+1] = 0xd9;
+            data[i+2] = 0xcf;
+        }
+
+        await sharp(data, {raw: {width: info.width, height: info.height, channels: info.channels}}).png().toFile('latest.png');
+        
+        interaction.reply({files: ['latest.png']});
+    }
+}
